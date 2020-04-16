9bc7e9f081bcfa082af6363a197c22c46781b8ae
dsalice <dsalice@ajou.ac.kr>
2020-04-16 12:05:05 +0900

    app.js 수정

--- App.js
+++ App.js
@@ -1,53 +1,51 @@
-import React from 'react';
+import React from "react";
+import { Alert } from "react-native";
 import Loading from "./Loading";
-import {Alert} from "react-native";
 import * as Location from "expo-location";
 import axios from "axios";
 import Weather from "./Weather";
 
-const API_KEY = "9e6fd0217927594656c041fa68336288";
+const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
 
 export default class extends React.Component {
   state = {
     isLoading: true
   };
-  getWeather = async(latitude, longitude) => {
+  getWeather = async (latitude, longitude) => {
     const {
-      data:{
-        main:{temp},
+      data: {
+        main: { temp },
         weather
       }
     } = await axios.get(
       `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
     );
     this.setState({
-      isLoading:false, 
-      condition:weather[0].main,
-    temp});
+      isLoading: false,
+      condition: weather[0].main,
+      temp
+    });
   };
-  getLocation = async() => {
+  getLocation = async () => {
     try {
       await Location.requestPermissionsAsync();
       const {
-        coords: {latitude, longitude}}
-         = await Location.getCurrentPositionAsync();
-      // Send to API and get weather
-      this.getWeather(latitude, longitude)
-      this.setState({isLoading: false});
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
     } catch (error) {
       Alert.alert("Can't find you.", "So sad");
     }
   };
-  componentDidMount(){
-   this.getLocation();
+  componentDidMount() {
+    this.getLocation();
   }
   render() {
-    const {isLoading, temp, condition} = this.state;
-    return isLoading? (
-    <Loading />
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? (
+      <Loading />
     ) : (
-    <Weather temp={Math.round(temp)} condition={condition}/>
+      <Weather temp={Math.round(temp)} condition={condition} />
     );
   }
 }
-
